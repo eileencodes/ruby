dcae9e10c7ee6673ce72630412d6d70a3eb29804
Aaron Patterson <tenderlove@ruby-lang.org>
2023-02-16 16:13:17 -0800

    Count unique shapes + hit & misses at each PC
    This counts the number of unique inline cache hit and misses at each
    program counter.
    
    Use it like this:
    
    ```ruby
    RubyVM.start_logging_ic_data
    
    ... do some stuff
    
    RubyVM.stop_logging_ic_data
    
    File.open("out.csv", "w") { |f| RubyVM.dump_ic_info f }
    ```

--- vm_core.h
+++ vm_core.h
@@ -670,6 +670,9 @@ typedef struct rb_vm_struct {
     rb_shape_t *shape_list;
     rb_shape_t *root_shape;
     shape_id_t next_shape_id;
+    st_table * ic_stats;
+    st_table * pc_to_iseq;
+    bool log_ic_info;
 
     /* load */
     VALUE top_self;
